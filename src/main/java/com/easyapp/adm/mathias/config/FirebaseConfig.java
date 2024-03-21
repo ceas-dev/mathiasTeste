4ef427af4b6a76cf18b542f4ed39a44fcb198d56
ceas <edu.alves1327@gmail.com>
2024-03-21 16:53:08 -0300

    Adcionando variavies de ambiente no config do firebase

--- src/main/java/com/easyapp/adm/mathias/config/FirebaseConfig.java
+++ src/main/java/com/easyapp/adm/mathias/config/FirebaseConfig.java
@@ -6,18 +6,26 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
     @Bean
     FirebaseApp firebaseApp() throws IOException {
-        var serviceAccount = new FileInputStream("src/main/resources/credentials/firebase.json");
-        var options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://mathiasadm-b5772-default-rtdb.firebaseio.com")
-                .build();
-        return FirebaseApp.initializeApp(options);
+        // Lê as variáveis de ambiente
+        String credentialsEnv = System.getenv("FIREBASE_CREDENTIALS");
+        String databaseUrlEnv = System.getenv("FIREBASE_DATABASE_URL");
+
+        // Configura as opções do Firebase
+        FirebaseOptions.Builder optionsBuilder = FirebaseOptions.builder();
+        optionsBuilder.setDatabaseUrl(databaseUrlEnv);
+
+        // Carrega as credenciais a partir de uma string em JSON
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new ByteArrayInputStream(credentialsEnv.getBytes()));
+        optionsBuilder.setCredentials(credentials);
+
+        // Inicializa o Firebase App
+        return FirebaseApp.initializeApp(optionsBuilder.build());
     }
 }
